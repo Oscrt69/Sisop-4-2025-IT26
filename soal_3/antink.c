e2d369610da146c5bb5491031c9ce39589d46a19
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2025-05-20 06:05:50 +0700

    Update antink.c

--- soal_3/antink.c
+++ soal_3/antink.c
@@ -1,132 +1,135 @@
 #define FUSE_USE_VERSION 31
 #include <fuse3/fuse.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <ctype.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <dirent.h>
 #include <time.h>
+#include <ctype.h>
+#include <dirent.h>
 
-const char *src_path;
-const char *log_path = "/var/log/it24.log";
+static const char *host_dir = "/it24_host";
+static const char *log_path = "/antink-system/antink-logs/it24.log";
 
-void log_event(const char *type, const char *filename) {
-    FILE *log = fopen(log_path, "a");
-    if (!log) return;
-
-    time_t now = time(NULL);
-    struct tm *t = localtime(&now);
-    fprintf(log, "[%04d-%02d-%02d %02d:%02d:%02d] %s: %s\n",
-        t->tm_year+1900, t->tm_mon+1, t->tm_mday,
-        t->tm_hour, t->tm_min, t->tm_sec, type, filename);
-    fclose(log);
+void reverse_name(char *filename) {
+    int len = strlen(filename);
+    for (int i = 0; i < len/2; i++) {
+        char temp = filename[i];
+        filename[i] = filename[len-1-i];
+        filename[len-1-i] = temp;
+    }
 }
 
-char *reverse_name(const char *name) {
-    int len = strlen(name);
-    char *rev = malloc(len+1);
-    for (int i = 0; i < len; i++)
-        rev[i] = name[len - i - 1];
-    rev[len] = 0;
-    return rev;
+void log_activity(const char *action, const char *filename) {
+    time_t now;
+    time(&now);
+    char timestr[20];
+    strftime(timestr, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+    
+    FILE *log_file = fopen(log_path, "a");
+    if (log_file) {
+        if (strcmp(action, "ALERT") == 0) {
+            fprintf(log_file, "[%s] [ALERT] Anomaly detected %s in file: %s\n", timestr, strstr(filename, "nafis") ? "nafis" : "kimcun", filename);
+        } else if (strcmp(action, "REVERSE") == 0) {
+            char reversed[256];
+            strcpy(reversed, filename);
+            reverse_name(reversed);
+            fprintf(log_file, "[%s] [REVERSE] File %s has been reversed : %s\n", timestr, filename, reversed);
+        } else if (strcmp(action, "ENCRYPT") == 0) {
+            fprintf(log_file, "[%s] [ENCRYPT] File %s has been encrypted\n", timestr, filename);
+        } else {
+            fprintf(log_file, "[%s] %s: %s\n", timestr, action, filename);
+        }
+        fclose(log_file);
+    }
 }
 
 int is_dangerous(const char *filename) {
-    return strstr(filename, "nafis") || strstr(filename, "kimcun");
+    char lower[256];
+    for (int i = 0; filename[i]; i++) {
+        lower[i] = tolower(filename[i]);
+    }
+    return strstr(lower, "nafis") || strstr(lower, "kimcun");
 }
 
-static int x_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags) {
-    char full_path[1024];
-    snprintf(full_path, sizeof(full_path), "%s%s", src_path, path);
+void rot13(char *str) {
+    for (; *str; str++) {
+        if ((*str >= 'a' && *str <= 'm') || (*str >= 'A' && *str <= 'M')) {
+            *str += 13;
+        } else if ((*str >= 'n' && *str <= 'z') || (*str >= 'N' && *str <= 'Z')) {
+            *str -= 13;
+        }
+    }
+}
 
+static int antink_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
+    char full_path[1024];
+    snprintf(full_path, sizeof(full_path), "%s%s", host_dir, path);
+    int res = lstat(full_path, stbuf);
+    if (res == -1) return -errno;
+    return 0;
+}
+
+static int antink_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags) {
+    char full_path[1024];
+    snprintf(full_path, sizeof(full_path), "%s%s", host_dir, path);
     DIR *dp = opendir(full_path);
     if (!dp) return -errno;
-
     struct dirent *de;
     while ((de = readdir(dp)) != NULL) {
-        char *name = strdup(de->d_name);
-        if (is_dangerous(name)) {
-            char *rev = reverse_name(name);
-            filler(buf, rev, NULL, 0, 0);
-            free(rev);
-        } else {
-            filler(buf, name, NULL, 0, 0);
+        struct stat st;
+        memset(&st, 0, sizeof(st));
+        st.st_ino = de->d_ino;
+        st.st_mode = de->d_type << 12;
+        char display_name[256];
+        strcpy(display_name, de->d_name);
+        if (is_dangerous(display_name)) {
+            reverse_name(display_name);
+            log_activity("REVERSE", de->d_name);
         }
-        free(name);
+        if (filler(buf, display_name, &st, 0, 0)) break;
     }
-
     closedir(dp);
     return 0;
 }
 
-static int x_getattr(const char *path, struct stat *stbuf,
-                     struct fuse_file_info *fi) {
-    char real[1024];
-    snprintf(real, sizeof(real), "%s%s", src_path, path);
-    if (is_dangerous(path)) {
-        char *rev = reverse_name(path);
-        snprintf(real, sizeof(real), "%s/%s", src_path, rev);
-        free(rev);
-    }
-    int res = lstat(real, stbuf);
-    if (res == -1)
-        return -errno;
-    return 0;
-}
-
-static int x_open(const char *path, struct fuse_file_info *fi) {
+static int antink_open(const char *path, struct fuse_file_info *fi) {
     char full_path[1024];
-    snprintf(full_path, sizeof(full_path), "%s%s", src_path, path);
-    if (is_dangerous(path)) {
-        log_event("ALERT", path);
-        char *rev = reverse_name(path);
-        snprintf(full_path, sizeof(full_path), "%s/%s", src_path, rev);
-        free(rev);
-    } else {
-        log_event("READ", path);
-    }
-
-    int fd = open(full_path, fi->flags);
-    if (fd == -1)
-        return -errno;
-
-    fi->fh = fd;
+    snprintf(full_path, sizeof(full_path), "%s%s", host_dir, path);
+    int res = open(full_path, fi->flags);
+    if (res == -1) return -errno;
+    close(res);
+    log_activity("OPEN", path);
     return 0;
 }
 
-static int x_read(const char *path, char *buf, size_t size, off_t offset,
-                  struct fuse_file_info *fi) {
-    int res = pread(fi->fh, buf, size, offset);
-
+static int antink_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    char full_path[1024];
+    snprintf(full_path, sizeof(full_path), "%s%s", host_dir, path);    
+    int fd = open(full_path, O_RDONLY);
+    if (fd == -1) return -errno;
+    int res = pread(fd, buf, size, offset);
+    if (res == -1) res = -errno;
     if (!is_dangerous(path)) {
-        for (size_t i = 0; i < res; ++i) {
-            if (isalpha(buf[i])) {
-                if ((buf[i] >= 'A' && buf[i] <= 'Z'))
-                    buf[i] = ((buf[i] - 'A' + 13) % 26) + 'A';
-                else if ((buf[i] >= 'a' && buf[i] <= 'z'))
-                    buf[i] = ((buf[i] - 'a' + 13) % 26) + 'a';
-            }
-        }
+        rot13(buf);
+        log_activity("ENCRYPT", path);
+    } else {
+        log_activity("ALERT", path);
     }
-
+    close(fd);
     return res;
 }
 
-static struct fuse_operations x_oper = {
-    .readdir = x_readdir,
-    .getattr = x_getattr,
-    .open = x_open,
-    .read = x_read,
+static struct fuse_operations antink_oper = {
+    .getattr = antink_getattr,
+    .readdir = antink_readdir,
+    .open = antink_open,
+    .read = antink_read,
 };
 
 int main(int argc, char *argv[]) {
-    src_path = argv[1];
-    char *mountpoint = argv[2];
-
-    char *fuse_argv[] = { argv[0], mountpoint, "-f" };
-    return fuse_main(3, fuse_argv, &x_oper, NULL);
+    umask(0);
+    return fuse_main(argc, argv, &antink_oper, NULL);
 }
